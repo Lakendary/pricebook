b55f384933a7a38f0261edccbeab6c0ace579091
Jade Rickerts <jade.rickerts@gmail.com>
2019-03-11 20:29:25 +0200

    add store validator rule for not empty name.

---
+++ PriceBookClassLibrary/Validators/StoreValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceBookClassLibrary.Validators
+{
+    public class StoreValidator : AbstractValidator<StoreModel>
+    {
+        public StoreValidator()
+        {
+            RuleFor(s => s.Name).NotEmpty();
+        }
+    }
+}
