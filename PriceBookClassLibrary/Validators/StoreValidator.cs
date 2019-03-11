7794abc71822c4f13a72675146b31527b2ca2867
Jade Rickerts <jade.rickerts@gmail.com>
2019-03-11 21:48:15 +0200

    update store validator with more rules for store name.

--- PriceBookClassLibrary/Validators/StoreValidator.cs
+++ PriceBookClassLibrary/Validators/StoreValidator.cs
@@ -11,7 +11,18 @@ namespace PriceBookClassLibrary.Validators
     {
         public StoreValidator()
         {
-            RuleFor(s => s.Name).NotEmpty();
+            RuleFor(s => s.Name)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty().WithMessage("The store name cannot be empty.")
+                .Length(2, 50).WithMessage("The store name must have 2 to 50 characters.")
+                .Must(beAValidStoreName).WithMessage("The store name has invalid characters.");
+        }
+
+        protected bool beAValidStoreName(string storeName)
+        {
+            storeName = storeName.Replace(" ", "");
+            storeName = storeName.Replace("-", "");
+            return storeName.All(Char.IsLetter);
         }
     }
 }
